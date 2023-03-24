12af4ca9bf4276162fa973491f93e0a6342c6ee2
Tyler Daerden <daerdentyler@gmail.com>
2023-03-24 14:53:58 +0100

    Ajout de région et préparation pour acceui exos 2l

--- Exos24032023/Program.cs
+++ Exos24032023/Program.cs
@@ -44,9 +44,43 @@ Console.ReadLine();
 
 #region Exos 2 Opérateurs
 
+#region 2.1 Année Bissextile
+
 // 2.1
+// Demandez à l'utilisateur d'entrer une année et retournez si cette année est bissextile ou non 
 
 
+#endregion
+
+#region 2.2 Calcul div , modulo , 2 entiers
+
+// 2.2
+//Calcul de la division entière, du modulo et de la division de deux entiers.
+//Résultat attendu pour 5 et 2 ➡ Division entière : 2, Modulo: 1, Division: 2,5.
+
+
+#endregion
+
+
+#region 2.3 Check BBAN
+
+// 2.3
+//Vérification d’un compte bancaire BBAN, si le compte est bon affichez OK sur la console sinon KO.
+//Le modulo des 10 premiers chiffres par 97 donne les 2 derniers. Sauf si le modulo = 0 dans ce cas les 2 derniers chiffres son t 9 7.
+//(utilisez la méthode « Substring » de la classe « string »).
+
+
+#endregion
+
+
+#region 2.4 BBAN into IBAN
+
+//2.4//Transformer un compte bancaire BBAN Belge (xxx xxxxxxx xx) en IBAN(BExx xxxx xxxx xxxx). Trouvez la démarche via un moteur de
+//recherche.
+
+
+
+#endregion
 
 
 
